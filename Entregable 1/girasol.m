001c2c6e2f2e36f67ca9289503093d175658fdd4
Samuel <ingenierofiestero@gmail.com>
2015-02-18 23:11:23 +0100

    Update girasol.m

--- Entregable 1/girasol.m
+++ Entregable 1/girasol.m
@@ -1,10 +1,10 @@
 function girasol(N)
-S='bo';
-d=137.51;
-n=1:N;
-r=sqrt(n);
-teta=pi*d*n/180;
-plot(r.*cos(teta),r.*sin(teta),S);
+  S='bo';
+  d=137.51;
+  n=1:N;
+  r=sqrt(n);
+  teta=pi*d*n/180;
+  plot(r.*cos(teta),r.*sin(teta),S);
 end
 
 
