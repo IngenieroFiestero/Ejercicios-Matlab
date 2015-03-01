070b5a93de4424b1ea55549c46d6a6e4c0abfca6
Samuel <ingenierofiestero@gmail.com>
2015-03-01 22:10:07 +0100

    Menos lineas

--- Entregable 1/girasol.m
+++ Entregable 1/girasol.m
@@ -1,10 +1,8 @@
 function girasol(N)
-  S='bo';
-  d=137.51;
   n=1:N;
   r=sqrt(n);
-  teta=pi*d*n/180;
-  plot(r.*cos(teta),r.*sin(teta),S);
+  teta=pi*137.51*n/180;
+  plot(r.*cos(teta),r.*sin(teta),'bo');
 end
 
 
