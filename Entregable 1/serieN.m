93bc8926e78449dc5f2a86713d3dd23055cf4226
Samuel <ingenierofiestero@gmail.com>
2015-03-04 02:23:23 +0100

    Updated

--- Entregable 1/serieN.m
+++ Entregable 1/serieN.m
@@ -1,12 +1,18 @@
-function nError = serieN(error)
-format long;
+function [nError] = serieN(error)
+nError = [0;0];
+format long e;
 n=1;
 er = 1;
 while error < er
     atan = 4*(6*arcotangente(n,1/8) + 2*arcotangente(n,1/57) + arcotangente(n,1/239));
     er = abs(pi - atan);
     if error > er
-        nError = n;
+        format short;
+        n
+        nError(1) = n;
+        format long e;
+        er
+        nError(2) = er;
         break
     end
     n = n+1;
