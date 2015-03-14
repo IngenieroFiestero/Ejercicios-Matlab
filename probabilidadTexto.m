86cd5115bf9efee0ba754987abedf19433d50186
Samuel <ingenierofiestero@gmail.com>
2015-03-14 15:35:35 +0100

    Update probabilidadTexto.m

--- probabilidadTexto.m
+++ probabilidadTexto.m
@@ -1,9 +1,7 @@
 function prob = probabilidadTexto(texto)
-sum = 0;
 prob = zeros(1,max(texto));
 for i=1:1:length(texto)
     prob(texto(i)) = prob(texto(i)) + 1;
-    sum = sum + 1;
 end
-prob = prob/sum;
+prob = prob/length(texto);
 end
